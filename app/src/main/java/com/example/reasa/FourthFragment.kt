f634a3454f8d3e4710ace7e4618773cd403a509f
Domirando <maisiedev@gmail.com>
2023-04-13 13:25:00 +0500

    reasa

---
+++ app/src/main/java/com/example/reasa/FourthFragment.kt
@@ -0,0 +1,58 @@
+package com.example.reasa
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.reasa.databinding.FragmentFourthBinding
+import com.example.reasa.databinding.FragmentThirdBinding
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+
+class FourthFragment : Fragment() {
+
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private lateinit var binding: FragmentFourthBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentFourthBinding.inflate(inflater, container, false)
+
+        binding.button4.setOnClickListener {
+            findNavController().navigate(R.id.action_fourthFragment_to_fivesFragment)
+        }
+
+        return binding.root
+
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            FourthFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
