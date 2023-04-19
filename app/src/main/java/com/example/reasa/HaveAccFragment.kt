77cd89773e9a42d75877c37f5b6dbb7bba118bfb
User <mubinashukrullayeva@gmail.com>
2023-04-19 13:39:08 +0500

    jnk

--- app/src/main/java/com/example/reasa/HaveAccFragment.kt
+++ app/src/main/java/com/example/reasa/HaveAccFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.reasa.databinding.FragmentHaveAccBinding
+import com.example.reasa.databinding.FragmentLoginBinding
 
 
 private const val ARG_PARAM1 = "param1"
@@ -14,6 +17,8 @@ private const val ARG_PARAM2 = "param2"
 class HaveAccFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
+
+    private lateinit var binding: FragmentHaveAccBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +33,14 @@ class HaveAccFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_have_acc, container, false)
+        binding = FragmentHaveAccBinding.inflate(inflater, container, false)
+
+        binding.signup.setOnClickListener{
+            findNavController().navigate(R.id.action_loginFragment_to_createFragment)
+        }
+
+        return binding.root
+
     }
 
     companion object {
--- app/src/main/java/com/example/reasa/LoginFragment.kt
+++ app/src/main/java/com/example/reasa/LoginFragment.kt
@@ -1,10 +1,15 @@
 package com.example.reasa
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.reasa.databinding.FragmentLoginBinding
+import com.example.reasa.databinding.FragmentThirdBinding
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -12,6 +17,8 @@ private const val ARG_PARAM2 = "param2"
 class LoginFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
+
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +32,19 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        binding.signin.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_haveAccFragment)
+        }
+
+        binding.signup.setOnClickListener{
+            findNavController().navigate(R.id.action_loginFragment_to_createFragment)
+        }
+
+        return binding.root
     }
 
     companion object {
